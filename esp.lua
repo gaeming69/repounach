a4504b314656827a1c87c3d2c83024437fdc25c8
gaeming69 <113803833+gaeming69@users.noreply.github.com>
2026-03-05 21:16:30 -0600

    Create esp.lua

---
+++ esp.lua
@@ -0,0 +1,4 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/z4tt483/ItzXery.lua/main/Esp-Players.lua"))()
